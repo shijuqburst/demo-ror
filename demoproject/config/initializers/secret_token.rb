8adb0cfb0f0a1e0a02714fdafa43b7574e1527ca
shijup <shijup@qburst.com>
2013-05-06 11:21:52 +0530

    Inital commit for the application

---
+++ demoproject/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demoproject::Application.config.secret_token = '72180209795ac8daca60d9cca57fb2c8ab4b1534d19f0a13781ef81aacb21b46384a256bae2361f78d9256f803d3302106d274ca05e5038b871eeb735ac42752'
